4fc52bd99cceb34cfeaa0e978ad9a233560820b7
miararoy <roy@empiricalhire.com>
2019-09-15 01:05:54 +0300

    replace 1 true

--- feature_store/utils.py
+++ feature_store/utils.py
@@ -11,5 +11,5 @@ def generate_key_value_query(query, key, value):
     return """
         select * from ({q}) t
         where t.{k}='{v}'
-    """.format(q=query.replace("false", "0"),k=key, v=value)
+    """.format(q=query.replace("false", "0").replace("true", "1"),k=key, v=value)
 
